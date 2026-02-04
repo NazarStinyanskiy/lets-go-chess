7d9f8e2f12df32add536eebe28184fbd2d8448cc
NazarStinyanskiy <nazarstinyanskiy@gmail.com>
2026-02-04 23:48:00 +0200

    improved simple pawn move test

--- game/processor_test.go
+++ game/processor_test.go
@@ -5,19 +5,22 @@ import (
 	"testing"
 )
 
-func TestNextMoveSimple(t *testing.T) {
-	g1 := StartGame()
-	g2 := StartGame()
-	g2.isWhiteMove = false
-	tests := []struct {
-		from, to   Position
-		g          *Game
-		eField     Board
-		eSituation Situation
-		eError     error
-	}{
-		{from: Position{1, 2}, to: Position{1, 3}, g: g1, eField: *whitePawnMovedField(g1), eSituation: Continue, eError: nil},
-		{from: Position{1, 7}, to: Position{1, 6}, g: g2, eField: *blackPawnMovedField(g2), eSituation: Continue, eError: nil},
+type TestCase struct {
+	from, to   Position
+	g          *Game
+	eField     Board
+	eSituation Situation
+	eError     error
+}
+
+func TestNextMoveSimplePawnMove(t *testing.T) {
+	var tests []TestCase
+	for x := 1; x <= 8; x++ {
+		g := StartGame()
+		tests = append(tests, TestCase{from: Position{x, 2}, to: Position{x, 3}, g: g, eField: *whitePawnMovedField(g, x), eSituation: Continue, eError: nil})
+		g2 := StartGame()
+		g2.isWhiteMove = false
+		tests = append(tests, TestCase{from: Position{x, 7}, to: Position{x, 6}, g: g2, eField: *blackPawnMovedField(g2, x), eSituation: Continue, eError: nil})
 	}
 	for _, test := range tests {
 		situation, err := test.g.NextMove(test.from, test.to)
@@ -43,16 +46,16 @@ func isAllFiguresExpected(actual, expected Board) (bool, Position, *Figure) {
 	return true, Position{}, nil
 }
 
-func whitePawnMovedField(g *Game) *Board {
+func whitePawnMovedField(g *Game, x int) *Board {
 	field := copyField(g.Field)
-	field.Cells[Position{1, 3}] = field.Cells[Position{1, 2}]
-	field.Cells[Position{1, 2}] = nil
+	field.Cells[Position{x, 3}] = field.Cells[Position{x, 2}]
+	field.Cells[Position{x, 2}] = nil
 	return &field
 }
 
-func blackPawnMovedField(g *Game) *Board {
+func blackPawnMovedField(g *Game, x int) *Board {
 	field := copyField(g.Field)
-	field.Cells[Position{1, 6}] = field.Cells[Position{1, 7}]
-	field.Cells[Position{1, 7}] = nil
+	field.Cells[Position{x, 6}] = field.Cells[Position{x, 7}]
+	field.Cells[Position{x, 7}] = nil
 	return &field
 }
